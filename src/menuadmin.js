3d2204b30d71aded153565ff68ca0cc9c60f2466
marcosmods <78054628+marcosmods@users.noreply.github.com>
2021-03-01 20:43:29 -0300

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,25 @@
+const menuadmin = (prefix, pushname) => {
+ return `    K҉ I҉T҉O҉B҉O҉T҉  D҉O҉M҉IN҉A҉
+ da adm pro bot faz isso
+ 
+ ◪ COMANDO DOS ADMINS
+▻${prefix}bemvindo 1 (manda a tag quando alguem entra)
+▻${prefix}marcar (marca todos)
+▻${prefix}d (apaga msg do bot)
+▻${prefix}leave (bot mete o pé)
+▻${prefix}leveling (liga o level)
+▻${prefix}add 5528999...
+▻${prefix}promover @
+▻${prefix}fechargrupo
+▻${prefix}abrirgrupo
+▻${prefix}demote @
+▻${prefix}kick @
+▻${prefix}ping
+▻${prefix}level
+▻${prefix}link
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
